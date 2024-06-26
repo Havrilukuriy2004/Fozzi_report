1ac2902034c65fad3c8ad3e6b06326271a7ae991
Yurii Havryliuk <48659763+Havrilukuriy2004@users.noreply.github.com>
2024-06-26 14:20:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,36 +5,29 @@ import requests
 from io import BytesIO
 import datetime
 
-# Загрузка данных из Excel по URL
 @st.cache_data
 def load_data(url):
     response = requests.get(url)
     df = pd.read_excel(BytesIO(response.content), engine='openpyxl')
     return df
 
-# Фильтрация данных на основе условий
 def filter_data(df, week, report_type):
     if 'account' not in df.columns or 'partner' not in df.columns:
         st.error("'account' или 'partner' колонки не найдены в данных.")
-        return pd.DataFrame()  # Возвращаем пустой DataFrame
-
-    # Фильтрация по типу отчета
+        return pd.DataFrame()
+    
     if report_type == 'со счетом':
         df_filtered = df[(df['week'] <= week) & (df['account'].str.lower() == 'да') & (df['partner'].str.lower() == 'да')]
     else:
         df_filtered = df[(df['week'] <= week) & (df['account'].str.lower() == 'нет') & (df['partner'].str.lower() == 'нет')]
 
-    # Удаление строк, где 'recipient' содержит любые из mask_keywords
     mask_keywords = ['банк', 'пумб', 'держ', 'обл', 'дтек', 'вдвс', 'мвс', 'дсу', 'дснс', 'дпс', 'митна', 'гук']
     df_filtered = df_filtered[~df_filtered['recipient'].str.contains('|'.join(mask_keywords), case=False, na=False)]
-
-    # Удаление строк, где 'recipient' содержит 'район', кроме случаев, когда он содержит 'крайон'
     df_filtered = df_filtered[~df_filtered['recipient'].str.contains('район', case=False, na=False) | 
                               df_filtered['recipient'].str.contains('крайон', case=False, na=False)]
     
     return df_filtered
 
-# Добавление "прочих" и общей суммы к топ-10 спискам
 def add_others_and_total(data, col_name):
     top_data = data.nlargest(10, col_name)
     others_sum = data[~data.index.isin(top_data.index)][col_name].sum()
@@ -43,14 +36,12 @@ def add_others_and_total(data, col_name):
     top_data.loc['Gross Total'] = total_sum
     return top_data
 
-# Функция для получения диапазона дат для заданной недели
 def get_date_range_for_week(week_number, year):
     first_day_of_year = datetime.datetime(year, 1, 1)
     monday = first_day_of_year + datetime.timedelta(weeks=int(week_number) - 1, days=-first_day_of_year.weekday())
     sunday = monday + datetime.timedelta(days=6)
     return monday, sunday
 
-# Создание дашборда
 def create_dashboard(df):
     st.sidebar.header("Фильтры")
     selected_week = st.sidebar.selectbox("Выберите неделю", sorted(df['week'].unique()))
@@ -66,7 +57,6 @@ def create_dashboard(df):
     start_date_str = start_date.strftime('%d.%m.%Y')
     end_date_str = end_date.strftime('%d.%м.%Y')
 
-    # Заголовок и стили
     st.markdown(f"""
         <div style="background-color:#FFA500;padding:10px;border-radius:10px">
             <h1 style="color:white;text-align:center;">Платежи на крупных контрагентов ФОЗЗИ за пределы Востока за период {start_date_str} - {end_date_str}</h1>
@@ -76,11 +66,9 @@ def create_dashboard(df):
 
     st.header("Динамика платежей")
     if not filtered_data.empty:
-        # Фильтруем данные для динамики до выбранной недели включительно
         dynamics_data = df[df['week'] <= selected_week].groupby('week')['sum'].sum().reset_index()
         st.line_chart(dynamics_data.set_index('week'))
 
-        # Создаем сводную таблицу для получателей
         recipient_totals = filtered_data.groupby("recipient")["sum"].sum()
         top_10_recipients = recipient_totals.nlargest(10).index
 
@@ -88,7 +76,6 @@ def create_dashboard(df):
         recipients_pivot = recipients_pivot.loc[top_10_recipients]
         recipients_pivot['Total'] = recipients_pivot.sum(axis=1)
 
-        # Добавляем строку для "Прочих"
         other_data = filtered_data[~filtered_data["recipient"].isin(top_10_recipients)]
         other_totals = other_data.groupby('week')['sum'].sum()
         other_totals['Total'] = other_totals.sum()
@@ -107,38 +94,26 @@ def create_dashboard(df):
 
     st.header("Матрица Поставщик-Плательщик")
     if not filtered_data.empty:
-        # Группируем по получателям и суммируем суммы
         recipient_totals = filtered_data.groupby("recipient")["sum"].sum().reset_index()
-
-        # Выбираем топ-10 получателей
         top_10_recipients = recipient_totals.sort_values(by="sum", ascending=False).head(10)["recipient"]
 
-        # Группируем по плательщикам и суммируем суммы
         payer_totals = filtered_data.groupby("payer")["sum"].sum().reset_index()
-
-        # Выбираем топ-10 плательщиков
         top_10_payers = payer_totals.sort_values(by="sum", ascending=False).head(10)["payer"]
 
-        # Создаем сводную таблицу
         summary_data = []
-
         for recipient in top_10_recipients:
             recipient_data = filtered_data[filtered_data["recipient"] == recipient]
             row = [recipient] + [recipient_data[recipient_data["payer"] == payer]["sum"].sum() for payer in top_10_payers] + [recipient_data["sum"].sum()]
             summary_data.append(row)
 
-        # Добавляем строку для "Прочих"
         other_data = filtered_data[~filtered_data["recipient"].isin(top_10_recipients)]
         other_row = ["Others"] + [other_data[other_data["payer"] == payer]["sum"].sum() for payer in top_10_payers] + [other_data["sum"].sum()]
 
-        # Добавляем итоги
         totals_row = ["Total"] + [filtered_data[filtered_data["payer"] == payer]["sum"].sum() for payer in top_10_payers] + [filtered_data["sum"].sum()]
 
-        # Объединяем все строки
         summary_data.append(other_row)
         summary_data.append(totals_row)
 
-        # Создаем DataFrame для сводной таблицы
         try:
             summary_df = pd.DataFrame(summary_data, columns=["Recipient"] + top_10_payers.tolist() + ["Total"])
             st.table(summary_df)
@@ -149,51 +124,14 @@ def create_dashboard(df):
 
     st.header("Топ поставщиков")
     if not filtered_data.empty:
-        supplier_data = add_others_and_total(filtered_data.groupby('payer')['sum'].sum().reset_index(), 'sum')
-        st.table(supplier_data)
+        supplier_totals = filtered_data.groupby("payer")["sum"].sum().nlargest(10).reset_index()
+        st.table(supplier_totals)
     else:
         st.write("Нет данных для выбранных фильтров.")
 
-    if st.button("Скачать отчет в формате Excel"):
-        output_excel(filtered_data, selected_week, selected_report_type, start_date_str, end_date_str)
-
-def output_excel(df, week, report_type, start_date, end_date):
-    with pd.ExcelWriter('financial_report.xlsx') as writer:
-        dynamics_data = df[df['week'] <= week].groupby('week')['sum'].sum().reset_index()
-        dynamics_data.to_excel(writer, sheet_name='Динамика', index=False)
-
-        supplier_data = df.groupby(['week', 'payer'])['sum'].sum().reset_index()
-        supplier_data.to_excel(writer, sheet_name='Платежи по поставщикам', index=False)
-
-        matrix_data = df.pivot_table(values='sum', index='payer', columns='recipient', aggfunc='sum', fill_value=0)
-        top_suppliers = add_others_and_total(matrix_data.sum(axis=1).reset_index(), 0).index
-        top_payers = add_others_and_total(matrix_data.sum(axis=0).reset_index(), 0).index
-
-        matrix_data.loc['Others'] = matrix_data.loc[~matrix_data.index.isin(top_suppliers)].sum()
-        matrix_data.loc['Gross Total'] = matrix_data.sum()
-        matrix_data['Others'] = matrix_data[~matrix_data.columns.isin(top_payers)].sum(axis=1)
-        matrix_data['Gross Total'] = matrix_data.sum(axis=1)
-
-        top_suppliers = top_suppliers.tolist() + ['Others', 'Gross Total']
-        top_payers = top_payers.tolist() + ['Others', 'Gross Total']
-        matrix_data_filtered = matrix_data.loc[top_suppliers, top_payers]
-
-        matrix_data_filtered.to_excel(writer, sheet_name='Матрица', index=True)
-
-    st.write("Отчет успешно создан: [скачать отчет](financial_report.xlsx)")
-
-# Конфигурация страницы и запуск приложения Streamlit
-st.set_page_config(layout="wide")
-st.title("Финансовый отчет")
-
-# Укажите URL вашего Excel файла
-excel_url = "https://raw.githubusercontent.com/Havrilukuriy2004/Fozzi_report/main/raw_data_for_python_final.xlsx"
-df = load_data(excel_url)
-
-if not df.empty:
-    create_dashboard(df)
-else:
-    st.error("Не удалось загрузить данные. Проверьте URL и попробуйте снова.")
+    output_excel(filtered_data, selected_week, selected_report_type, start_date_str, end_date_str)
 
 if __name__ == "__main__":
-    main()
+    url = "https://raw.githubusercontent.com/Havrilukuriy2004/Fozzi_report/main/raw_data_for_python_final.xlsx"
+    df = load_data(url)
+    create_dashboard(df)
